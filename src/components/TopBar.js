d58bf4e67381821cf1d92b998e810721cc6b74a7
Gerald <gprmarquez@gmail.com>
2021-04-21 23:14:05 -0700

    changing AppBar component name to TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function ButtonAppBar() {
+export default function TopBar() {
     const classes = useStyles();
 
     return (
